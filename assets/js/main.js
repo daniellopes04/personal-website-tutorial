6bb7a923430fb768a172d54bf00b82d599adcd5a
daniellopes04 <daniel.lopes04@gmail.com>
2021-06-04 14:30:04 -0300

    Blob and home info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,28 @@
-// Show and hide menu constants
+// Show and hide mobile menu constants
 const navMenu = document.getElementById('nav-menu')
 const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
 
-// Show menu
+// Show mobile menu
 if(navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-// Hide menu
+// Hide mobile menu
 if(navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
+
+// Hide mobile menu when a link is clicked
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
