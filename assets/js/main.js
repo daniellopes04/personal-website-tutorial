48c7c487859566e81dbabd8f97f7b306475e2087
daniellopes04 <daniel.lopes04@gmail.com>
2021-06-04 13:07:15 -0300

    Showing and hiding menu

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// Show and hide menu constants
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+// Show menu
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Hide menu
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
