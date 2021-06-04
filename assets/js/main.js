f6a931a2fb7c162f5d96ca257b82a8760c2c1b5c
daniellopes04 <daniel.lopes04@gmail.com>
2021-06-04 17:56:50 -0300

    Adding skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,23 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Accordion skills
+const skillsContent = document.getElementsByClassName('skills-content')
+const skillsHeader = document.querySelectorAll('.skills-header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills-content skills-close'
+    }
+
+    if (itemClass === 'skills-content skills-close') {
+        this.parentNode.className = 'skills-content skills-open'
+    }
+}
+
+skillsHeader.forEach(elem => {
+    elem.addEventListener('click', toggleSkills)
+})
