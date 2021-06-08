03a054e29cee268fa4a4c9a6d5fc2f91d0b9a758
daniellopes04 <daniel.lopes04@gmail.com>
2021-06-07 21:16:33 -0300

    Adding portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,17 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+// Portfolio Swiper
+let swiper = new Swiper(".portfolio-container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
